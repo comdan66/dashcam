db97c846c8a1d0f77e32f04312eed2b3f19f547d
OA Wu <oa_wu@hiiir.com>
2015-08-18 17:55:33 +0800

    Temp

--- application/views/frame/site/frame.php
+++ application/views/frame/site/frame.php
@@ -12,24 +12,34 @@
   </head>
   <body lang="zh-tw">
     <?php echo isset ($hidden_list) ? $hidden_list : ''; ?>
-    
+
     <nav>
-      <div class='logo'>LOGO</div>
-      <div class='search'>
-        <div class='bar'>
-          <input type='text' name='main_search' id='main_search' value='' placeholder='搜尋！' />
-          <label for='main_search' class='icon-search'></label>
+      <div class='container'>
+        <div class='logo'>LOGO</div>
+        <div class='search'>
+          <div class='bar'>
+            <input type='text' name='main_search' id='main_search' value='' placeholder='搜尋！' />
+            <label for='main_search' class='icon-search'></label>
+          </div>
+        </div>
+        <div class='option icon-more' onClick='this.classList.toggle("show");' onmouseleave='this.classList.remove("show");'>
+          <div class='cover'></div>
+          <div class='menu i4'>
+            <a onClick='console.error (12);'>asd</a>
+            <a>asd</a>
+            <a>asd</a>
+            <a class='top_line'>asd</a>
+          </div>
         </div>
       </div>
-      <div class='option icon-menu'></div>
     </nav>
-    
+
     <div class='main_ball'>+</div>
 
     <div id='container'>
       <div class='wrapper'>
         <div class='wrapper_left'>
-          
+
           <div class='title'>主選單</div>
           <div class='group'>
             <a href='' class='icon-home'>首頁</a>
@@ -39,7 +49,7 @@
             <a href='' class='icon-search'>搜尋</a>
             <a href='' class='icon-menu'>選單</a>
           </div>
-          
+
           <div class='title'>次選單</div>
           <div class='group'>
             <a href='' class='icon-home'>首頁</a>
@@ -47,7 +57,7 @@
             <a href='' class='icon-menu'>選單</a>
             <a href='' class='icon-menu'>選單</a>
           </div>
-          
+
         </div>
         <div class='wrapper_right'>s</div>
       </div>
